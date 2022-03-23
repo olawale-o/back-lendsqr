34bc8d8b2108990e49a204c4c950f24ac94b5743
olawale-o <omoogunolawale@gmail.com>
2022-03-23 12:31:55 +0100

    Create server

---
+++ server.js
@@ -0,0 +1,11 @@
+import { env } from './constants';
+const { PORT } = env;
+
+const http = require('http');
+
+const app = require('./config/app');
+const server = http.createServer(app);
+
+server.listen(PORT || 3000, () => {
+  console.log('Server started on port 3000');
+});
