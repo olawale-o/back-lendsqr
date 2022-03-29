82e80a7a6dc7a96eafd8ea61423e3566a85bc64d
olawale-o <omoogunolawale@gmail.com>
2022-03-29 13:37:15 +0100

    Create more constant keys

--- constants/index.js
+++ constants/index.js
@@ -1,9 +1,10 @@
 
-require('dotenv').config()
-const { PORT } = process.env;
+require('dotenv').config();
+const { PORT, JWT_SECRET } = process.env;
 
 module.exports = {
   env: {
     PORT,
+    JWT_SECRET,
   },
 };
