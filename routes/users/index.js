1748257288c024d8e22888eb5c64fdff550a1f82
olawale-o <omoogunolawale@gmail.com>
2022-04-01 16:02:20 +0100

    Add enpoint to deposit cash

--- routes/users/index.js
+++ routes/users/index.js
@@ -4,5 +4,6 @@ const userController = require('../../controllers/users_controller');
 
 router.post('/register', userController.createUser);
 router.post('/login', userController.authenticateUser);
+router.post('/:id/deposit', userController.deposit);
 
 module.exports = router;
